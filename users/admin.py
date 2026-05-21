36870871edb2d0ff63252a1bc55c9cdf2ecf2193
Cristian <cristianmesa13@gmail.com>
2026-05-21 18:30:58 -0300

    se modifico el admin del usuario

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,25 @@
+# users/admin.py
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from users.models import Usuario
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
 @admin.register(Usuario)
-class UserAdmin(BaseUserAdmin):
-    pass
+class UsuarioAdmin(UserAdmin):
+    # Campos que se muestran en la lista
+    list_display = ['username', 'email', 'rol', 'first_name', 'last_name', 'is_active']
+    list_filter  = ['rol', 'is_active']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
 
+    # Agregar los campos custom al formulario de edición
+    fieldsets = UserAdmin.fieldsets + (
+        ('Información del restaurante', {
+            'fields': ('rol', 'telefono')
+        }),
+    )
+
+    # Agregar los campos custom al formulario de creación
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Información del restaurante', {
+            'fields': ('rol', 'telefono')
+        }),
+    )
