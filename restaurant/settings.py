0da29121d2c31ec562f4650c08f69826c99c83f9
Cristian <cristianmesa13@gmail.com>
2026-05-31 19:54:24 -0300

    actualice el cors_allowed_origins

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
 # ✅ Correcto
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://localhost:5174', 
     'http://localhost:3000',
     'https://dashboard-restaurante-theta.vercel.app',  # sin /login al final
 ]
